bb951355f00ed6c17a3d575e778ac79c2d2c5cb1
Josh Maggard <jmaggard62@gmail.com>
2020-04-17 20:06:17 -0500

    Initial Commit

---
+++ Assets/Butler/Editor/ButlerSettings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Editor.ButlerScripts
+{
+
+    [CreateAssetMenu(fileName = "ButlerSettings", menuName = "Butler/Create ButlerSettings", order = 1)]
+    public class ButlerSettings : ScriptableObject
+    {
+        private static ButlerSettings butlerAsset;
+        public static ButlerSettings Instance
+        {
+            get
+            {
+                if (butlerAsset == null)
+                    butlerAsset = AssetDatabase.LoadAssetAtPath<ButlerSettings>("Assets/Butler/ButlerSettings.asset");
+                if (butlerAsset == null)
+                    throw new Exception("Unable to find ButlerSettings asset file");
+                return butlerAsset;
+            }
+        }
+
+        public string GameName = "game-name";
+        public string UserName = "username";
+        public bool BuildAndroid = false;
+        public bool BuildWeb = false;
+        public bool BuildPC = false;
+
+        public string KeystorePassword = "";
+        public string KeystoreAlias = "";
+        public string KeystoreAliasPassword = "";
+    }
+}
---
+++ pushToMaster.bat
@@ -0,0 +1,1 @@
+git checkout master && git merge --no-ff -m %1 develop && git push && git checkout develop
