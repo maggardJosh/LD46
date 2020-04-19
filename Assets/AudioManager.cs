7f741e906af92f95bda97b1dc759da34664c0d80
Josh Maggard <jmaggard62@gmail.com>
2020-04-19 14:55:20 -0500

    Music

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Entity.Base;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager _instance;
+    private static AudioManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindObjectOfType<AudioManager>();
+            return _instance;
+        }
+    }
+
+    public AudioSource sfxAudioSource;
+    public AudioSource musicAudioSource;
+
+    private bool MuteMusic
+    {
+        get
+        {
+            return PlayerPrefs.GetInt("MuteMusic", 0) == 1;
+        }
+        set
+        {
+            PlayerPrefs.SetInt("MuteMusic", value ? 1 : 0);
+        }
+    }
+
+    void Awake()
+    {
+        _instance = this;
+        musicAudioSource.volume = MuteMusic ? 0 : .3f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ToggleMusicSound();
+        }
+    }
+
+    public static void PlayOneShot(AudioClip ac)
+    {
+        if (!Instance.sfxAudioSource.isPlaying)
+            Instance.sfxAudioSource.pitch = 1.0f + Random.Range(-GameSettings.RandomSFXAmount, GameSettings.RandomSFXAmount);
+        Instance.sfxAudioSource.PlayOneShot(ac);
+    }
+
+    public void ToggleMusicSound()
+    {
+        MuteMusic = !MuteMusic;
+        Instance.musicAudioSource.volume = MuteMusic ? 0 : .3f;
+    }
+
+}
--- Assets/Entity/Base/GameSettings.cs
+++ Assets/Entity/Base/GameSettings.cs
@@ -8,8 +8,10 @@ namespace Entity.Base
         public float tileSize = 16;
         public float collisionOffsetValue = .1f;
         public float gravity = 1f;
+        public  float randomSFXAmount = .1f;
 
         private static GameSettings _instance;
+
         private static GameSettings Instance
         {
             get
@@ -22,5 +24,6 @@ namespace Entity.Base
         public static float TileSize { get => Instance.tileSize; }
         public static float CollisionOffsetValue { get => Instance.collisionOffsetValue; }
         public static float Gravity { get => Instance.gravity; }
+        public static float RandomSFXAmount { get => Instance.randomSFXAmount;}
     }
 }
