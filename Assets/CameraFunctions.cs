92dd5222528659568f37cf2d24c3e78a34df6a71
Josh Maggard <jmaggard62@gmail.com>
2020-04-19 19:43:12 -0500

    Final touches

--- Assets/CameraFunctions.cs
+++ Assets/CameraFunctions.cs
@@ -4,7 +4,7 @@ public static class CameraFunctions{
     public static bool IsOnScreen(Vector3 pos)
     {
         var viewportPoint = Camera.main.WorldToViewportPoint(pos);
-        const float flexAmount = .3f;
+        const float flexAmount = .4f;
         bool onScreen = viewportPoint.x >= -flexAmount && viewportPoint.x <= 1 + flexAmount && viewportPoint.y >= -flexAmount && viewportPoint.y <= 1 + flexAmount;
         return onScreen;
     }
